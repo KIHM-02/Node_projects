0963df084a2b0eee18648bc8908b3e68cf80b88b
KIHM-02 <monteroisaac33@gmail.com>
2024-11-23 14:43:19 -0600

    Added the first example of commonjs

---
+++ commonjs/index.js
@@ -0,0 +1,3 @@
+const sum = require("./sum"); //Aqui se hace la importacion 
+
+console.log(`El resultado es ${sum(5, 15)} y su tipo es ${typeof sum(5, 15)}`);
---
+++ commonjs/sum.js
@@ -0,0 +1,6 @@
+function sum(a, b){
+    return a+b;
+}
+
+//De esta manera exportamos el modulo
+module.exports = sum;
