267232ee5932e4b701a21913a09aca0fcb77d3ff
KIHM-02 <monteroisaac33@gmail.com>
2024-11-24 01:02:26 -0600

    Changed the file's extension from .js to .mjs

--- modulejs/index.mjs
+++ modulejs/index.mjs
@@ -1,6 +1,6 @@
 // El mjs es el metodo de modulos mas moderno que se utiliza para exportar funciones
 
-import {sum, sub, div, mul} from './sum.js';
+import {sum, sub, div, mul} from './sum.mjs';
 
 console.log(sum(5, 9));
 console.log(mul(5, 3));
